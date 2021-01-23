bbfd48ea923502a614342c217cadf7847a38582e
정효인 <hobbyhyoin@naver.com>
2021-01-23 14:50:19 +0000

    21.1.23

---
+++ src/hash/hashlv3_1.cpp
@@ -0,0 +1,42 @@
+#include <string>
+#include <vector>
+#include <map>
+#include <algorithm>
+#include <iostream>
+
+using namespace std;
+bool cmp(const pair<string,int>& a,const pair<string,int>& b)
+{
+    return a.second > b.second;
+}
+
+vector<int> solution(vector<string> genres, vector<int> plays) {
+    vector<int> answer;
+    vector<string> Sgenres(genres);
+    multimap <string,int> hashmap;
+    map <string,int> shashmap;
+    for(int i=0;i<genres.size();i++)
+    {
+        hashmap.insert(pair(genres[i],plays[i]));
+    }
+  
+    sort(Sgenres.begin(),Sgenres.end());
+    Sgenres.erase(unique(Sgenres.begin(),Sgenres.end()),Sgenres.end());
+    multimap<string,int>::iterator iter;
+    for(int i=0;i<Sgenres.size();i++)
+    {
+        int sum=0;
+        for(iter=hashmap.lower_bound(Sgenres[i]);iter!=hashmap.upper_bound(Sgenres[i]);iter++)
+        {
+           sum+=(iter->second);
+        }
+        shashmap[Sgenres[i]]=sum;
+    }
+    vector<pair<string,int>> vec(shashmap.begin(), shashmap.end());//pop 3100 classic 1450
+    sort(vec.begin(),vec.end(),cmp);
+    for(int i=0;i<2;i++)
+    {
+        
+    }
+    return answer;
+}
